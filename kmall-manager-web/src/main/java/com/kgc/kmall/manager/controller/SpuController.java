c50c37737d1cac5d203a21f7d961e49c51f53510
mayishuo <3525801900@qq.com>
2020-12-21 18:26:20 +0800

    图片上传测试成功

--- kmall-manager-web/src/main/java/com/kgc/kmall/manager/controller/SpuController.java
+++ kmall-manager-web/src/main/java/com/kgc/kmall/manager/controller/SpuController.java
@@ -4,7 +4,13 @@ import com.kgc.kmall.bean.PmsBaseSaleAttr;
 import com.kgc.kmall.bean.PmsProductInfo;
 import com.kgc.kmall.bean.PmsProductSaleAttr;
 import com.kgc.kmall.service.SpuService;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.dubbo.config.annotation.Reference;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,6 +21,8 @@ import java.util.List;
 public class SpuController {
     @Reference
     SpuService spuService;
+    @Value("${fileServer.url}")
+    String fileUrl;
     @RequestMapping("/spuList")
     public List<PmsProductInfo> supList(Long catalog3Id){
         return spuService.spuList(catalog3Id);
@@ -24,10 +32,29 @@ public class SpuController {
        return spuService.baseSaleAttrList();
     }
     @RequestMapping("/fileUpload")
-    public String fileUpload(@RequestParam("file")MultipartFile file){
+    public String fileUpload(@RequestParam("file")MultipartFile file) throws Exception{
         //文件上传
         //返回文件上传后的路径
-        return "https://m.360buyimg.com/babel/jfs/t5137/20/1794970752/352145/d56e4e94/591417dcN4fe5ef33.jpg";
+        String imgUrl=fileUrl;
+        if(file!=null){
+            System.out.println("multipartFile="+file.getName()+"|"+file.getSize());
+            String configFile=this.getClass().getResource("/tracker.conf").getFile();
+            ClientGlobal.init(configFile);
+            TrackerClient trackerClient=new TrackerClient();
+            TrackerServer trackerServer=trackerClient.getTrackerServer();
+            StorageClient storageClient=new StorageClient(trackerServer,null);
+            String filename=    file.getOriginalFilename();
+            String extName = FilenameUtils.getExtension(filename);
+
+            String[] upload_file = storageClient.upload_file(file.getBytes(), extName, null);
+            imgUrl=fileUrl ;
+            for (int i = 0; i < upload_file.length; i++) {
+                String path = upload_file[i];
+                imgUrl+="/"+path;
+            }
+        }
+        System.out.println(imgUrl);
+        return imgUrl;
     }
     @RequestMapping("/saveSpuInfo")
     public String saveSpuInfo(@RequestBody PmsProductInfo pmsProductInfo){
--- kmall-manager-web/src/test/java/com/kgc/kmall/manager/KmallManagerWebApplicationTests.java
+++ kmall-manager-web/src/test/java/com/kgc/kmall/manager/KmallManagerWebApplicationTests.java
@@ -1,5 +1,9 @@
 package com.kgc.kmall.manager;
 
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +11,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 class KmallManagerWebApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		String file = this.getClass().getResource("/tracker.conf").getFile();
+		ClientGlobal.init(file);
+		TrackerClient trackerClient=new TrackerClient();
+		TrackerServer trackerServer=trackerClient.getTrackerServer();
+		StorageClient storageClient=new StorageClient(trackerServer,null);
+		String orginalFilename="C:\\Users\\20254\\Desktop\\123.jpg";
+		String[] upload_file = storageClient.upload_file(orginalFilename, "jpg", null);
+		String path="http://192.168.60.132";
+		for (int i = 0; i < upload_file.length; i++) {
+			String s = upload_file[i];
+			System.out.println("s = " + s);
+			path+="/"+s;
+		}
+		System.out.println(path);
 	}
 
 }
