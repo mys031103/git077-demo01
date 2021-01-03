7274f6598cd2e548dc74fb21f52c0fb73f298ea5
mayishuo <3525801900@qq.com>
2021-01-03 10:21:47 +0800

    前端详情页面缓存成功

--- kmall-manager-service/src/main/java/com/kgc/kmall/manager/mapper/PmsSkuInfoMapper.java
+++ kmall-manager-service/src/main/java/com/kgc/kmall/manager/mapper/PmsSkuInfoMapper.java
@@ -28,4 +28,5 @@ public interface PmsSkuInfoMapper {
     int updateByPrimaryKeySelective(PmsSkuInfo record);
 
     int updateByPrimaryKey(PmsSkuInfo record);
+    List<PmsSkuInfo> selectBySpuId(Long spuId);
 }
